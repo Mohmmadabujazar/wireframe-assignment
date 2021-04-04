cc69b96996c1866949bb290e5a9748fd372a7f47
Mohmmadabujazar <mohmmadabujazar@yahoo.com>
2021-04-04 21:14:23 +0300

    editing file myweb.js

--- myweb.js
+++ myweb.js
@@ -1,3 +1,4 @@
+/*
 alert("welcome in my web");
 alert("I hope to learn new something every day");
 var username =prompt("please enter your name");
@@ -12,3 +13,25 @@ else if (car== "mazda") {document.write("<img src='https://wheelz.me/wp-content/
 else if (car== "lada") {document.write("<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQON3sTKnkZSImpY0csm3LC_hD9WIB9Ry_wCnS0y4hFFod0QHRhwvBJt0goxbHlUvxW3Fo&usqp=CAU' />")
 }
 
+*/
+
+var password = prompt("please enter your password");
+while(password !=='123'){
+    password = prompt("your passward is wrong please try again");
+}
+
+var cats = prompt("please enter a number how many cats you want? from 1 till 5");
+for (var i = 0; i < cats; i++){
+    if(password=='123'){
+        document.write('<img src="https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg" />');
+    } else {
+        document.write('<img src="https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png" />');
+    }
+}
+function cat(param1, param2 ){
+for (var i = param1; i < param2; i++){
+    console.log(i); 
+    console.log(i*2)
+    }
+}
+cat(1,5);
